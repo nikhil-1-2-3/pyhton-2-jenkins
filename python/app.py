0feb2466317b5840a60764b1baa0f2f5d696dd5d
nikhil singh <23amtics402@gmail.com>
2026-03-29 11:23:37 +0530

    Replace name input loop with command line argument

--- python/app.py
+++ python/app.py
@@ -1,15 +1,4 @@
-file_name = "names.txt"
+import sys
 
-while True:
-    name = input("Enter a name (or type 'exit' to stop): ")
-
-    if name.lower() == 'exit':
-        break
-
-    with open(file_name, "a") as f:
-        f.write(name + "\n")
-
-    print("\nNames entered so far:")
-    with open(file_name, "r") as f:
-        print(f.read())
-    print("-" * 20)
+name = sys.argv[1] if len(sys.argv) > 1 else "Default"
+print("Hello", name)
